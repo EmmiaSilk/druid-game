cbe5469f931cc3f91c3bfdb57883c54281caee5a
Emmia Silk <emmiasilk@gmail.com>
2023-06-21 20:57:39 -0400

    Removed unused content from module.js

--- wasm-frontend/www/module.js
+++ wasm-frontend/www/module.js
@@ -1,19 +1,6 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
-export function drawFrame(x, y, width, height, buffer) {
-    let imageData = ctx.createImageData(width, height);
-    // TODO: Fill buffer with image data from the buffer
-    for(let i = 0; i < imageData.data.length; i += 4) {
-        imageData.data[i + 0] = 190;
-        imageData.data[i + 1] = 0;
-        imageData.data[i + 2] = 210;
-        imageData.data[i + 3] = 255;
-    }
-    
-    ctx.putImageData(imageData, x, y);
-}
-
 export function image_data_from_bitmap(ctx, bitmap) {
     let imageData = ctx.createImageData(bitmap.width, bitmap.height);
     let colors = bitmap.colors;
@@ -29,8 +16,6 @@ export function image_data_from_bitmap(ctx, bitmap) {
         imageData.data[i*4+2] = b;
         imageData.data[i*4+3] = a;
     }
-
-    console.log(bitmap);
 
     return imageData;
 }
